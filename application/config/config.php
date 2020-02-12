f6d04b952b3fdc8e96f33c93ca7029f517150a95
Bima S. Ramadhan <37492909+bimaramadhan@users.noreply.github.com>
2020-02-12 23:55:55 +0700

    coba

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PBKK-tugas1-biodata';
+$config['base_url'] = 'https://bimaramadhan.github.io/PBKK-tugas1-biodata';
 
 /*
 |--------------------------------------------------------------------------
